cd84cd11d74e9cce6e005f7b7a6b8b4f848f82cf
platinhom <zhaozxcpu@hotmail.com>
2015-10-08 03:19:24 +0800

    kplane_update

--- python/algorithm/kplane.py
+++ python/algorithm/kplane.py
@@ -4,18 +4,29 @@
 #######################################################################################
 This program implements the K-plane regression algorithm based on EM algorithm 
 to give the optimal partitioning of the data set in the sense ofd minimize the RMS
-
 error of the regression. Together with the SVM classifer, we can do the classification.
+
+Return:
 rms is the total rms error
-coef is the regression coefficients for each plane
+coef is the regression coefficients for each group
+partition_groups is the group information for each data
+
+Input:
 x is the training set; x(1:num_samples, 1:number_fearures)
 y is the target value: y(1:num_samples)
-num_group is the number of groups
-dgroup is the initial partitioning
-lamda is the regularization constant
+dgroup is the initial group information for each data if given
 
-Author: Bao Wang, wangbaonj@gmail.com, 09/30/2015 based on Matlab
-Reviser: Zhixiong Zhao, 10/06/2015 based on python
+Parameters:
+num_group is the number of groups
+regroup is whether use regroup method to make sure the result with starting group number.
+randomgroup is whether use randomized (or well-ordering method) group information in initialization.
+print_in is whether print out the intermediate information.
+klamda is the regularization constant for k-plane
+rlamda is the regularization constant for least sqaure regression
+rmslimit is the convergence limit for RMS 
+
+Author: Bao Wang, wangbaonj@gmail.com, 09/30/2015 based on Matlab: basical algorithm
+Reviser: Zhixiong Zhao, 10/06/2015 based on python: objective, regroup algorithm
 ########################################################################################
 '''
 
@@ -30,7 +41,7 @@ class KPlaneObj():
 		pass
 
 	def CheckData(self,data=True,group=True):
-		# Check input data
+		# Check input data and convert them to valid data type
 		if (data):
 			if (self.x.ndim==1):
 				self.x=self.x.T
@@ -61,12 +72,20 @@ class KPlaneObj():
 				print "Automatically set group number to "+str(int(self.dlen/2))
 				self.SetGroupNumber(int(self.dlen/2));
 
+	def SetAllParameters(self,klamda=0.001,rlamda=500, rmslimit=0.0001):
+		self.klamda=klamda
+		self.rlamda=rlamda
+		self.rmslimit=rmslimit
+
 	def SetGroupNumber(self,number=1):
 		self.num_group=number;
 		# May need further dgroup assignment
 
-	# grouping function can use only after initialize
-	# Grouping Based on sequence
+	def SetGroupData(self,dgroup):
+		self.dgroup=dgroup;
+
+	### Grouping function can use only after initialize
+	#   Grouping Based on sequence
 	def AutoGroup(self):
 		num_group=self.num_group
 		dlen=self.dlen
@@ -74,13 +93,15 @@ class KPlaneObj():
 		for k in range(0,num_group-1):
 			self.dgroup[k*np.floor(dlen/num_group):(k+1)*np.floor(dlen/num_group)]=k+1;
 		self.dgroup[(num_group-1)*np.floor(dlen/num_group):dlen]=num_group;
-	# Grouping Based on randomize
+
+	#   Grouping Based on randomize
 	def RandomGroup(self):
 		for i in range(self.dlen):
 			self.dgroup[i]=np.random.randint(1,num_group+1);
 
-	# Initialize based on x and y data
-	def initializeByData(self,x,y,num_group=1,lamda=0.001,print_in=False,randomgroup=False):
+	### Initialization on different input data
+	#   Initialize based on x and y data (array or list or tuple)
+	def initializeByData(self,x,y,num_group=1,regroup=True,randomgroup=False,print_in=False):
 		try:
 			if (isinstance(x,np.ndarray)):
 				self.x=x
@@ -90,22 +111,31 @@ class KPlaneObj():
 				self.y=y
 			else:
 				self.y=np.array(y)
+			# Set default parameters in calculation
+			self.SetAllParameters();
+			# Set number of group, whether show intermediate information
 			self.num_group=num_group
-			self.lamda=lamda
 			self.print_in=print_in
+			# Check and adjust the data.
 			self.CheckData();
+
 			self.dgroup=np.zeros((self.dlen,1),dtype='int32')
 			self.centers=np.zeros((self.num_group,self.plen),dtype='float64')
+			if (regroup):
+				self.regroup=True;
+			else: 
+				self.regroup=False;
 			if (randomgroup):
 				self.RandomGroup();
 			else:
 				self.AutoGroup();
+
 		except:
 			traceback.print_exc()
-			#exit()
+			exit()
 
-	# Initialize based on data file
-	def initializeByFile(self,filename='data.txt',num_group=1,lamda=0.001,print_in=False,randomgroup=False):
+	#   Initialize based on data file (with y x data)
+	def initializeByFile(self,filename='data.txt',num_group=1,regroup=True,randomgroup=False,print_in=False):
 		#load data from file
 		try:
 			data=np.loadtxt(filename) #dlen(m) data * plen(n) para + 1
@@ -114,12 +144,31 @@ class KPlaneObj():
 			assert (dlen>1 and plen>1)
 			ymat=data[:,0];
 			xmat=data[:,1:]
-			self.initializeByData(xmat,ymat,num_group,lamda,print_in,randomgroup)
+			self.initializeByData(xmat,ymat,num_group,regroup,randomgroup,print_in)
 		except:
 			traceback.print_exc()
 			exit()
 
-	def LeastSqaures(self,x,y,lamda=500):
+	#   Initialize based on data file with group information (with group y x data)
+	def initializeByFileWithGroup(self,filename='data.txt',num_group=1,regroup=True,print_in=False):
+		#load data from file
+		try:
+			data=np.loadtxt(filename) #dlen(m) data * plen(n) para + 1 y + 1 group
+			dlen=data.shape[0]
+			plen=data.shape[1]
+			assert (dlen>1 and plen>2)
+			gmat=data[:,0];
+			ymat=data[:,1];
+			xmat=data[:,2:]
+			# Use random group method (True here) to initial may be faster
+			self.initializeByData(xmat,ymat,num_group,regroup,True,print_in)
+			self.SetGroupData(gmat);
+		except:
+			traceback.print_exc()
+			exit()
+
+
+	def LeastSqaures(self,x,y,rlamda=500):
 		try:
 			if (x.ndim==1):
 				mx=x.size;
@@ -138,11 +187,11 @@ class KPlaneObj():
 
 			if (mx==my and ny==1):
 				wts=np.zeros((1,nx))
-				wts[0, :]=(np.linalg.inv(x.T.dot(x)+lamda*np.eye(nx)).dot(x.T.dot(y))).T
+				wts[0, :]=(np.linalg.inv(x.T.dot(x)+rlamda*np.eye(nx)).dot(x.T.dot(y))).T
 				return wts
 			elif( mx==ny and my==1):
 				wts=np.zeros((1,nx))
-				wts[0, :]=(np.linalg.inv(x.T.dot(x)+lamda*np.eye(nx)).dot(x.T.dot(y.T))).T
+				wts[0, :]=(np.linalg.inv(x.T.dot(x)+rlamda*np.eye(nx)).dot(x.T.dot(y.T))).T
 			else:
 				raise ValueError, "Not match array size for x and y!";
 		except:
@@ -152,7 +201,7 @@ class KPlaneObj():
 	def CalcRMSArray(self,arr):
 		return np.sqrt((arr.T.dot(arr)).ravel()[0]/arr.shape[0])
 
-	def UpdateCenters(self,ReGroup=False):
+	def UpdateCenters(self):
 		updateHere=False # Whether need to update rms
 		for i in range(self.num_group):
 			try:
@@ -160,7 +209,7 @@ class KPlaneObj():
 				#print gdataIndex
 				if (len(gdataIndex)>=2):
 					self.centers[i, :]=sum(self.x.take(gdataIndex,axis=0))/(len(gdataIndex)); 
-				elif (len(gdataIndex)==1 and ReGroup):
+				elif (len(gdataIndex)==1 and self.regroup):
 					maxerrn=np.argmax(self.errors)
 					self.errors[maxerrn]=0.0
 					tmplist=gdataIndex.tolist()
@@ -169,11 +218,14 @@ class KPlaneObj():
 					# Whether need to update rms
 					updateHere = True
 				elif (len(gdataIndex)==0):
-					if (ReGroup):
+					if (self.regroup):
+						print "Group disappear: "+ str(i+1)
 						if (self.print_in): print "Group disappear: "+ str(i+1)
 						maxerrn=np.argmax(self.errors)
+						print self.errors[maxerrn]
 						self.errors[maxerrn]=0.0
 						maxerrn2=np.argmax(self.errors)
+						print self.errors[maxerrn2]
 						self.errors[maxerrn2]=0.0
 						self.centers[i, :]=sum(self.x.take((maxerrn,maxerrn2),axis=0))/2;
 						self.partition_groups[maxerrn]=i+1
@@ -187,7 +239,7 @@ class KPlaneObj():
 				print "Still error at centers calculation.."
 		return updateHere
 
-	def UpdateRegression(self,lamda=500):
+	def UpdateRegression(self):
 		for i in range(self.num_group):
 			#500*eye(n) is the regularization in the regression
 			dgindex=np.where(self.partition_groups.ravel()==i+1)[0]
@@ -195,21 +247,22 @@ class KPlaneObj():
 			tmpy=self.y.take(dgindex,axis=0)
 			# Regression
 			#wts[i, :]=(np.linalg.inv( tmpx.T.dot(tmpx)+500*np.eye(n) ).dot(tmpx.T.dot(tmpy)) ).T;
-			self.wts[i, :]=self.LeastSqaures(tmpx,tmpy,lamda); 
+			self.wts[i, :]=self.LeastSqaures(tmpx,tmpy,self.rlamda); 
 
 			#Regression error for each data
 			#err.take(dgindex,axis=0)=(np.abs(tmpy.T-(wts[i, :].dot(tmpx.T)).T)).T;
 			# Instead method:
 			absnow=(np.abs(tmpy.T-(self.wts[i, :].dot(tmpx.T)).T)).T
 			for j in range(len(absnow)):
+				if (self.errors[dgindex[j]]==0.0):print absnow[j]
 				self.errors[dgindex[j]]=absnow[j];
 		rms=self.CalcRMSArray(self.errors);
 		return rms;
 
-	def kplane(self,ReGroup=False):
+	# The main kpane function here
+	def kplane(self):
 		rms1=10.0; rms2=0.0;
 		coef=0.0;
-		LeastSqauresLamda=500
 
 		x=self.x;
 		y=self.y;
@@ -231,12 +284,12 @@ class KPlaneObj():
 		wts=self.wts
 		err=self.errors;
 		err_temp=np.zeros((self.num_group, 1));
-		rms1=self.UpdateRegression(LeastSqauresLamda);
+		rms1=self.UpdateRegression();
 		# Averge x center value for each group
 		centers=self.centers;
-		self.UpdateCenters(ReGroup);
+		self.UpdateCenters();
 		# Update rms if using regroup method
-		if(ReGroup):rms1=self.UpdateRegression(LeastSqauresLamda);
+		if(self.regroup):rms1=self.UpdateRegression();
 
 		if self.print_in: print "Starting RMS: " + str(rms1)
 
@@ -245,46 +298,48 @@ class KPlaneObj():
 		minstartcount=0
 		oldminrms=9999.0
 		# Update the partition and regressor via EM algorithm
-		while( np.abs(rms1-rms2)>0.0001 or rms1<rms2 or (np.abs(rms1-rms2)<=0.0001 and ReGroup )):
+		while( np.abs(rms1-rms2)>self.rmslimit or rms1<rms2 or \
+			(np.abs(rms1-rms2)<=self.rmslimit and self.regroup )):
+
 			rms1=rms2;
 			#Update the partition
 			for i in range(m):
 				for j in range(self.num_group):
 				#lambda*norm2(x(i, :)-centers(j, :)) is the regularization term
-					if (ReGroup):
-						err_temp[j]=abs(y[i]-(wts[j, :].dot(x[i, :].T)).T)+self.lamda*np.linalg.norm(x[i, :]-centers[j, :]);
+					if (self.regroup):
+						err_temp[j]=abs(y[i]-(wts[j, :].dot(x[i, :].T)).T)+self.klamda*np.linalg.norm(x[i, :]-centers[j, :]);
 					else:
 						err_temp[j]=abs(y[i]-(wts[j, :].dot(x[i, :].T)).T)
 						# discard contribution from regularization term because NaN errors.
 				dgroup[i]=np.argmin(err_temp)+1;
 
 			# Update regressor
-			rms2=self.UpdateRegression(LeastSqauresLamda);
+			rms2=self.UpdateRegression();
 			# Update the centers of each group
-			rmsUpdate2=self.UpdateCenters(ReGroup);	
+			rmsUpdate2=self.UpdateCenters();	
 			# Update rms if regroup in update center
 			if(rmsUpdate2):
-				rms2=self.UpdateRegression(LeastSqauresLamda);
+				rms2=self.UpdateRegression();
 
 			if self.print_in: print "Grouped state: "+ str(dgroup.T)
 			if self.print_in: print "RMS now: "+str(rms2)
 			print "RMS&Group: "+str(rms2)+ str(dgroup.T)
 
 			if (rms2<minrms):
-				if (minrms-rms2<0.0001):
+				if (minrms-rms2<self.rmslimit):
 					minrmscount+=1;
 				else: minrmscount=1;
 				minrms=rms2;
 				minstartcount=0;
 				print "New Min RMS: "+str(minrms)
-			if (rms2-minrms<0.0001 and rms2>=minrms):
+			if (rms2-minrms<self.rmslimit and rms2>=minrms):
 				minrmscount+=1;
 				#if (minrmscount>10):
 				#	print "Minrms: "+str(minrms)+" for "+str(minrmscount)
 			if (minrmscount>10):
 				break
 			# repeat doing
-			if (minstartcount>1000):
+			if (minstartcount>500):
 				oldminrms=minrms;
 				minrms=rms2;
 				print "Best Min RMS before changing MINRMS: "+str(oldminrms)
@@ -302,12 +357,16 @@ class KPlaneObj():
 
 if (__name__=="__main__"):
 	try:
-		# create Kplane object and initialize
+		# create Kplane object
 		kp=KPlaneObj();
-		kp.initializeByFile("data.txt",num_group=8,lamda=0.0,print_in=False,randomgroup=True);
-
-		#Update the partitioning by k-plane regression
-		rms, coef, partition_groups=kp.kplane(ReGroup=True);
+		# Initialize data by file and set up program
+		kp.initializeByFile("data.txt",num_group=8,regroup=True,randomgroup=False,print_in=False);
+		# Setup parameters. PS: initial will reset all default parameters, 
+		# you have to set them after initialization if you don't want to use default. 
+		# Default: klamda=0.001, rlamda=500,rmslimit=0.0001
+		kp.SetAllParameters(klamda=0,rlamda=500, rmslimit=0.0001);
+		#Calculate the partitioning by k-plane regression
+		rms, coef, partition_groups = kp.kplane();
 
 	except:
 		traceback.print_exc()
